9dc02264b3f8c148da7afabac07ee3ef539cc1c1
Ryan Arjun <karshan813@gmail.com>
2018-10-05 12:34:18 -0400

    Register user works

--- MusicShop/LoginForm.Designer.cs
+++ MusicShop/LoginForm.Designer.cs
@@ -28,11 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(12, 24);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(176, 20);
+            this.usernameTB.TabIndex = 0;
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(12, 72);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(176, 20);
+            this.passwordTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(12, 113);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.Location = new System.Drawing.Point(113, 113);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(75, 23);
+            this.registerBtn.TabIndex = 5;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "LoginForm";
+            this.ClientSize = new System.Drawing.Size(220, 160);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.usernameTB);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button registerBtn;
     }
 }
--- MusicShop/RegistrationForm.cs
+++ MusicShop/RegistrationForm.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace MusicShop
@@ -15,6 +11,61 @@ namespace MusicShop
         public RegistrationForm()
         {
             InitializeComponent();
+        }
+
+        private void registerSubmitBtn_Click(object sender, EventArgs e)
+        {
+            int uid = 0;
+            using (RNGCryptoServiceProvider RCSP = new RNGCryptoServiceProvider())
+            {
+                byte[] random = new byte[5];
+                RCSP.GetBytes(random);
+                uid = BitConverter.ToInt32(random, 0);
+            }
+            string fileName = @"C:\Users\KR\documents\visual studio 2015\Projects\MusicShop\MusicShop\UsersTable.csv";
+            string username = registerUsernameTB.Text;
+            string password = registerPasswordTB.Text;
+            string str = string.Format("{0},{1},{2},{3}", uid, username, password, 0);
+
+            List<string> lines = new List<String>();
+
+            // Open the file and then file the line and then delete it if found.
+            if (File.Exists(fileName))
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains(","))
+                        {
+                            // Split the line by ","
+                            string[] split = line.Split(',');
+                            // If the user id is in the third position of the array delete the line
+                            if (split[1].Contains(username)) {
+                                MessageBox.Show("Error Message", "Username exist", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            }
+                            else
+                            {
+                                string header = "uid,username,password,isAdmin" + Environment.NewLine;
+                                File.WriteAllText(fileName, header);
+                                File.AppendAllText(fileName, str);
+                            }
+                        }
+                        lines.Add(line);
+                    }
+                }
+
+                // Add the lines back tot eh file
+                using (StreamWriter writer = new StreamWriter(fileName, false))
+                {
+                    foreach (string l in lines)
+                        writer.WriteLine(l);
+                }
+            }
+            registerPasswordTB.Clear();
+            registerPasswordTB.Clear();
+            this.Close();
         }
     }
 }
